1a4199bd8a82235de61815d1204e8adbf46ce472
Joao Victor Evangelista Cruz <joaovecruz@yahoo.com.br>
2026-06-11 11:03:39 -0300

    Teste bem sucedido com apenas 1 asteroide

--- IA/evaluating.py
+++ IA/evaluating.py
@@ -5,7 +5,7 @@ print("Carregando o ambiente com interface gráfica...")
 env = AsteroidsEnv(render_mode="human")
 
 print("Carregando o modelo treinado...")
-model = PPO.load("ppo_10ast_changed_3M")
+model = PPO.load("ppo_1ast")
 
 obs, _ = env.reset()
 done = False
