610b9ef307acaf6e90381b5a9464812f4ff0ca0c
João Victor Evangelista Cruz <98784264+jao-07@users.noreply.github.com>
2026-09-24 10:25:51 -0300

    Add setup.py for asteroids_cpp module
    Setup script for building the asteroids_cpp module with Pybind11.

---
+++ IA/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+sources = [
+    "Source/Bindings.cpp",
+    "Source/Math.cpp",
+    "Source/Random.cpp",
+    "Source/Actors/Actor.cpp",
+    "Source/Components/Component.cpp",
+    "Source/Game.cpp",
+    "Source/Components/DrawComponent.cpp",
+    "Source/Components/RigidBodyComponent.cpp",
+    "Source/Actors/Ship.cpp",
+    "Source/Actors/Asteroid.cpp",
+    "Source/Components/CircleColliderComponent.cpp",
+    "Source/Actors/Laser.cpp",
+    "Source/Actors/Particle.cpp",
+]
+
+ext_modules = [
+    Pybind11Extension(
+        "asteroids_cpp",
+        sources,
+        include_dirs=[
+            "Source",
+        ],
+        libraries=["SDL2"],
+        cxx_std=17,
+    ),
+]
+
+setup(
+    name="asteroids_cpp",
+    version="1.0",
+    author="João",
+    description="Módulo C++ do jogo Asteroids com suporte a Pybind11",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+)
